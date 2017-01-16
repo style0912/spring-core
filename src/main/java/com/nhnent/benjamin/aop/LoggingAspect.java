0d049ee78e586da9cbb0c2dc54a4b757b3e46038
Benjamin <benjamin@Benjamins-MacBook-Pro.local>
2017-01-17 07:21:03 +0900

    add before, afterreturnning

--- src/main/java/com/nhnent/benjamin/aop/LoggingAspect.java
+++ src/main/java/com/nhnent/benjamin/aop/LoggingAspect.java
@@ -1,5 +1,9 @@
 package com.nhnent.benjamin.aop;
 
+import com.nhnent.benjamin.vo.Member;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -12,7 +16,24 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
 
     @Before("execution(* getMember(..))")
-    public void before() {
+    public void before(JoinPoint joinPoint) {
         System.out.println("Start Method! >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ");
+        Signature signature = joinPoint.getSignature();
+        System.out.println("Started Method : " + signature.getName());
+
+        Object[] args = joinPoint.getArgs();
+        StringBuilder sb = new StringBuilder();
+        for (Object arg : args){
+            sb.append(arg).append(",");
+        }
+        System.out.println("Started Method Arguments : " + sb.toString());
     }
+
+    @AfterReturning(value = "execution(* getMember(..))", returning = "member")
+    public void afterReturning(Member member){
+        System.out.println("After Method! >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        System.out.println("return value : " + member.toString());
+    }
+
+
 }
