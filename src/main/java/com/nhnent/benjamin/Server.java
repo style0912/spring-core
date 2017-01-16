2dd494d49f990f91bb0fda97aa73210683eb0440
Benjamin <benjamin@Benjamins-MacBook-Pro.local>
2017-01-16 22:10:00 +0900

    add close

--- src/main/java/com/nhnent/benjamin/Server.java
+++ src/main/java/com/nhnent/benjamin/Server.java
@@ -5,7 +5,6 @@ import com.nhnent.benjamin.dao.MemberDao;
 import com.nhnent.benjamin.vo.Member;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Date;
@@ -17,7 +16,7 @@ public class Server {
     private static final Logger LOGGER = LoggerFactory.getLogger(Server.class);
 
     public static void main(String args[]) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationContextConfig.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ApplicationContextConfig.class);
 
         MemberDao memberDao = context.getBean(MemberDao.class);
 
@@ -40,6 +39,8 @@ public class Server {
         } else {
             System.out.println(member.getName());
         }
+
+        context.close();
 
     }
 }
